bfe8f5e14bc06b57175bd40559630fffc44a7f63
Rafael Bankosegger <rafael@bankosegger.at>
2020-08-12 20:35:57 +0200

    re-implement return ratio

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-from tests import *
-from MonteCarlo import *
+from tests import test_policy
+from MonteCarlo import MonteCarlo
+from BlocksWorld import BlocksWorld
+
+from matplotlib import pyplot as plt
 
 blocks_world = BlocksWorld()
 mc = MonteCarlo(blocks_world, max_episode_length=8, planning_factor=0, plan_on_empty_policy=False, planning_horizon=0)
@@ -8,3 +11,6 @@ print(f'Learned policy: {learned_policy}')
 print()
 
 test_policy(learned_policy, blocks_world, 8)
+
+plt.plot(mc.return_ratios)
+plt.show()
