66a65b8961a48dce99a7f8106eb2896ab3b50804
Rafael Bankosegger <rafael@bankosegger.at>
2020-09-14 12:20:59 +0200

    Refactor monte-carlo control algorithms and add q-learning-based control algorithms

---
+++ control/__init__.py
@@ -0,0 +1,4 @@
+from .off_policy_control import OffPolicyControl
+from .monte_carlo import FirstVisitMonteCarloControl, MonteCarloSGDControl
+from .q_learning import QLearningControl, QLearningReversedUpdateControl
+
--- main.py
+++ main.py
@@ -1,27 +1,41 @@
-from tests import test_policy
-from MonteCarlo import MonteCarlo
-from mdp import BlocksWorldBuilder, VacuumCleanerWorldBuilder
-from control import SimpleMonteCarloControl, SgdMonteCarloControl
-from planner import Planner
+from mdp import BlocksWorldBuilder, VacuumCleanerWorldBuilder, SokobanBuilder
+from control import *
+from policy import *
 
 from matplotlib import pyplot as plt
+from tqdm import tqdm
+
+blocks_world_size = 4
+max_episode_length = 20
+number_episodes = 300
+planning_horizon = 3
+planning_factor = 0
+plan_for_new_states = True 
+
+mdp_builder = BlocksWorldBuilder(blocks_world_size)
+#mdp_builder = SokobanBuilder('suitcase-05-01')
+
+sample_mdp = mdp_builder.build_mdp()
+planner_policy = PlannerPolicy(planning_horizon, sample_mdp.interface_file_path,
+                               sample_mdp.problem_file_path, sample_mdp.state_static)
+
+target_policy= QTablePolicy()
+#behavior_policy = PlanningExploringStartsPolicy(planner_policy, RandomPolicy(), QTablePolicy(),
+#                                              planning_factor, plan_for_new_states)
+behavior_policy = PlanningEpsilonGreedyPolicy(planner_policy, RandomPolicy(), QTablePolicy(),
+                                            epsilon=0.2)
+
+#control = FirstVisitMonteCarloControl(behavior_policy)
+#control = MonteCarloSGDControl(target_policy, alpha=0.3)
+#control = QLearningReversedUpdateControl(target_policy, behavior_policy, alpha=0.3)
+control = QLearningControl(target_policy, behavior_policy, alpha=0.3)
 
 
-mdp_builder = BlocksWorldBuilder(blocks_world_size=7)
-#mdp_builder = VacuumCleanerWorldBuilder()
-planner = Planner(planning_horizon=12)
-ctrl = SimpleMonteCarloControl()
-mc = MonteCarlo(mdp_builder, planner, control=ctrl, 
-                max_episode_length=14, planning_factor=0.7, plan_on_empty_policy=False,
-                exploring_starts=True, exploring_factor = 0.0)
+returns = []
+for _ in tqdm(range(number_episodes), total=number_episodes):
+    mdp = mdp_builder.build_mdp()
+    control.learn_episode(mdp, step_limit=max_episode_length)
+    returns.append(mdp.return_history[0])
 
-learned_policy = mc.learn_policy(number_episodes=300, show_progress_bar=True, evaluate_return_ratio=True)
-
-# Get sub-policy statistics to make sure they work correctly
-n = float(sum(cnt for cnt in mc.count_sub_policies.values()))
-print([(pol, cnt / n) for pol, cnt in mc.count_sub_policies.items()])
-
-#test_policy(ctrl, blocks_world, mgenerate_random_state verbose=False)
-
-plt.plot(mc.returns)
+plt.plot(returns)
 plt.show()
