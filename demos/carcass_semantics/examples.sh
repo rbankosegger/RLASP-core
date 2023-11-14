6e6504018cc028ce9c75771f14e3bc94a3df7b68
Rafael Bankosegger <rafael@bankosegger.at>
2023-11-14 13:30:05 +0100

    give application demo of ASP-CARCASS semantics

--- demos/carcass_semantics/examples.sh
+++ demos/carcass_semantics/examples.sh
@@ -2,7 +2,7 @@
 
 # Example 1 - Simple online interaction
 # We expect one answer set. Extract one cStateChoice, many cActionCovers!
-clingo ps.lp pAs.lp project_online.lp car_ord.lp car_psi.lp 0
+clingo ps.lp pAs.lp car_ord.lp car_psi.lp 0
 
-# Example 2 - State-action pairs covered by an abstract state-actionpair
+# Example 2 - State-action pairs covered by an abstract state-action pair
 clingo psi.lp car_psi.lp restrict.lp project_states.lp project_actions.lp 0
